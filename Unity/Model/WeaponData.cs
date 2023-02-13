335f4d8bbf27748db78f3369fee37133213e83f6
Ahmao <suhsuan520@gmail.com>
2023-02-13 11:14:01 +0800

    Create WeaponData.cs

---
+++ Unity/Model/WeaponData.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+public class WeaponData
+{
+    public int Id;
+    public string Name;
+    public int Score;
+
+    //public string CustomerID;
+    //public string CompanyName;
+    //public string ContactName;
+    //public string ContactTitle;
+}
