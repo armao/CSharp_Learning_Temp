e619686513326b665c16eb4ce46927d953cc9fe1
Ahmao <suhsuan520@gmail.com>
2023-02-13 12:15:33 +0800

    Add files via upload

---
+++ DotNetFramework/Controllers/ProductsController.cs
@@ -0,0 +1,156 @@
+﻿using _06ADOnet.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace _06ADOnet.Controllers
+{
+      public class ProductsController : Controller
+      {
+            private NorthwindEntities db = new NorthwindEntities();
+            SetData setData = new SetData();
+
+            // GET: Products
+            public ActionResult Index()
+            {
+                  var products = db.Products.Include(p => p.Categories).Include(p => p.Suppliers);
+                  return View(products.ToList());
+            }
+
+            // GET: Products/Details/5
+            public ActionResult Details(int? id)
+            {
+                  if (id == null)
+                  {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                  }
+                  Products products = db.Products.Find(id);
+                  if (products == null)
+                  {
+                        return HttpNotFound();
+                  }
+                  return View(products);
+            }
+
+            // GET: Products/Create
+            public ActionResult _Create()
+            {
+                  ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+                  ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
+                  return PartialView();
+            }
+
+            // POST: Products/Create
+            // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+            // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Create(Products products)
+            {
+                  if (ModelState.IsValid)
+                  {
+
+                        string sql = "insert into Products(ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued) " +
+                        "values(@ProductName,@SupplierID,@CategoryID,@QuantityPerUnit,@UnitPrice,@UnitsInStock,@UnitsOnOrder,@ReorderLevel,@Discontinued)";
+                        //"values('" + products.ProductName + "', "+products.SupplierID+","+products.CategoryID+",'"+products.QuantityPerUnit+"')";
+
+                        List<SqlParameter> list = new List<SqlParameter> {
+
+                              new SqlParameter("ProductName", products.ProductName),
+                              new SqlParameter("SupplierID", products.SupplierID),
+                              new SqlParameter("CategoryID", products.CategoryID),
+                              new SqlParameter("QuantityPerUnit", products.QuantityPerUnit),
+                              new SqlParameter("UnitPrice", products.UnitPrice),
+                              new SqlParameter("UnitsInStock", products.UnitsInStock),
+                              new SqlParameter("UnitsOnOrder", products.UnitsOnOrder),
+                              new SqlParameter("ReorderLevel", products.ReorderLevel),
+                              new SqlParameter("Discontinued", products.Discontinued)
+
+                        };
+
+                        setData.executeSql(sql,list);
+
+                        //db.Products.Add(products);
+                        //db.SaveChanges();
+                        return RedirectToAction("Index");
+                  }
+
+                  ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+                  ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+                  return View(products);
+            }
+
+            // GET: Products/Edit/5
+            public ActionResult Edit(int? id)
+            {
+                  if (id == null)
+                  {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                  }
+                  Products products = db.Products.Find(id);
+                  if (products == null)
+                  {
+                        return HttpNotFound();
+                  }
+                  ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+                  ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+                  return View(products);
+            }
+
+            // POST: Products/Edit/5
+            // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+            // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
+            {
+                  if (ModelState.IsValid)
+                  {
+                        db.Entry(products).State = EntityState.Modified;
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                  }
+                  ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
+                  ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
+                  return View(products);
+            }
+
+            // GET: Products/Delete/5
+            public ActionResult Delete(int? id)
+            {
+                  if (id == null)
+                  {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                  }
+                  Products products = db.Products.Find(id);
+                  if (products == null)
+                  {
+                        return HttpNotFound();
+                  }
+                  return View(products);
+            }
+
+            // POST: Products/Delete/5
+            [HttpPost, ActionName("Delete")]
+            [ValidateAntiForgeryToken]
+            public ActionResult DeleteConfirmed(int id)
+            {
+                  Products products = db.Products.Find(id);
+                  db.Products.Remove(products);
+                  db.SaveChanges();
+                  return RedirectToAction("Index");
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                  if (disposing)
+                  {
+                        db.Dispose();
+                  }
+                  base.Dispose(disposing);
+            }
+      }
+}
